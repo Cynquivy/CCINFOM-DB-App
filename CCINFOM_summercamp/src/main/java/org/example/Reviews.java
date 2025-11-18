905c8ef742e52cceebfd2deca9137f864e61b7f3
Cynquivy <63828727+Cynquivy@users.noreply.github.com>
2025-11-18 12:00:07 +0800

    fixed runtime errors & redundancies in Reviews

--- CCINFOM_summercamp/src/main/java/org/example/Reviews.java
+++ CCINFOM_summercamp/src/main/java/org/example/Reviews.java
@@ -3,24 +3,25 @@ package org.example;
 import java.time.LocalDateTime;
 
 public class Reviews {
-    private int reviewID; 
-    private int camperID; 
-    private int counselorID; 
+    private int reviewID;
+    private int personID;
     private int rating;
     private String comments;
-    private LocalDateTime reviewDate; 
+    private LocalDateTime reviewDate;
 
-    public Reviews(){} 
-    public Reviews(int reviewID, int camperID, int counselorID, int rating, String comments, LocalDateTime reviewDate){
-        this.reviewID = reviewID; this.camperID = camperID;  this.counselorID = counselorID; this.rating = rating; this.comments = comments; this.reviewDate = reviewDate; 
+    public Reviews(){}
+    public Reviews(int reviewID, int personID, int rating, String comments, LocalDateTime reviewDate){
+        this.reviewID = reviewID;
+        this.personID = personID;
+        this.rating = rating;
+        this.comments = comments;
+        this.reviewDate = reviewDate;
     }
 
     public int getReviewID(){ return reviewID; }
     public void setReviewID(int reviewID){ this.reviewID = reviewID; }
-    public int getCamperID(){ return camperID; }
-    public void setCamperID(int camperID){ this.camperID = camperID; }
-    public int getCounselorID(){ return counselorID; } 
-    public void setCounselorID(int counselorID){ this.counselorID = counselorID; }
+    public int getPersonID(){ return personID; }
+    public void setPersonID(int personID){ this.personID = personID; }
     public int getRating(){ return rating; }
     public void setRating(int rating){ this.rating = rating; }
     public String getComments(){ return comments;}
